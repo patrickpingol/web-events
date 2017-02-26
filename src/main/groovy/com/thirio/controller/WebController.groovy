0de430c242ddf968e3c6d2039b659dee8cae490c
Patrick Maurice Pingol <Patrick Maurice Pingol>
2017-02-26 07:41:22 -0800

    Finished index page and added createEventsPage

--- src/main/groovy/com/thirio/controller/WebController.groovy
+++ src/main/groovy/com/thirio/controller/WebController.groovy
@@ -1,5 +1,7 @@
 package com.thirio.controller
 
+import com.thirio.model.Event
+import com.thirio.service.DatabaseConnection
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.CookieValue
@@ -10,12 +12,25 @@ import org.springframework.web.bind.annotation.RequestMapping
  */
 @Controller
 class WebController {
+    private DatabaseConnection dbcon = new DatabaseConnection()
 
     @RequestMapping( value = ['', '/', '/index'] )
     String homePage( Model model, @CookieValue( value = 'EVENTID', defaultValue = '' ) String eventId ) {
-
+        if ( eventId != '' ) {
+            Event event = dbcon.getEvent( Integer.parseInt( eventId.toString() ) )
+            model.addAttribute( 'eventName', event.name )
+            model.addAttribute( 'eventDate', event.date )
+        } else {
+            Event[] events = dbcon.getEventList( '', '' )
+            model.addAttribute( 'events', events )
+        }
 
         return 'index'
+    }
+
+    @RequestMapping( value = '/event/create' )
+    String createEventPage( Model model ) {
+        return 'create-event'
     }
 
     @RequestMapping( value = '/lottery' )
